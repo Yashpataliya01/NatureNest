e8954c4a720576a065dd114b486e4a78b297b036
yashpataliya01 <yashpataliya01@gmail.com>
2024-07-03 23:10:08 +0530

    add vercel file

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -6,39 +6,30 @@ import postRoutes from "./routes/post.route.js";
 import cors from "cors";
 import dotenv from "dotenv";
 import path from "path";
-dotenv.config({
-  path: "./.env",
-});
 
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("🗄️ MongoDB connected successfully"))
-.catch((err) => console.error("Failed to connect to MongoDB", err));
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL);
 
 const app = express();
 const __dirname = path.resolve();
 
-app.use(cors({
-  origin: "*",
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use("/api/auth", userRoutes);
-app.use("/api/post", postRoutes);
-
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend", "dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
   });
 }
 
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-  console.log(`⚙️ Server is running at port: ${PORT}`);
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
 });
+
+app.use("/api/auth", userRoutes);
+app.use("/api/post", postRoutes);
