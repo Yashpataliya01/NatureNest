580eeff97fc985bb479c76b587cff9b5e129ecb4
yashpataliya01 <yashpataliya01@gmail.com>
2024-07-04 08:17:59 +0530

    add vercel file

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -1,14 +1,32 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
         target: 'http://localhost:3002',
         secure: false,
+        changeOrigin: true,
       },
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Set the limit as per your requirement
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
   },
   plugins: [react()],
